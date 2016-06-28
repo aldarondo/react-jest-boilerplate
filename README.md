e3a8381de5226eee6bf220e87c069416312514dc
Aldarondo <gqu6ald@mgcc124539.ent.rt.csaa.com>
2016-06-28 08:43:58 -0700

    Initial Version

---
+++ README.md
@@ -0,0 +1,27 @@
+# React Jest Boilerplate
+=====================
+
+A minimal and light dev environment for ReactJS.
+
+### Usage
+
+```
+npm install
+npm start
+open http://localhost:8000
+```
+
+### Linting
+
+This boilerplate project includes React ESLint configuration.
+
+```
+npm run test
+```
+
+### Dependencies
+
+* React
+* Webpack
+* [babel-loader](https://github.com/babel/babel-loader)
+* [webpack-dev-server](https://github.com/webpack/webpack-dev-server)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8000',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css?$/,
+        loaders: [ 'style', 'raw' ],
+        include: __dirname
+      }
+    ]
+  }
+};
